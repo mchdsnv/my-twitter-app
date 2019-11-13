6aa5560dfa0f5d447b1d293b31ff11a1cfbbc72d
mchdsnv <56634006+mchdsnv@users.noreply.github.com>
2019-11-13 09:27:18 +0200

    Global style removed to separate file

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -6,55 +6,36 @@ import './App.css';
 
 import uuid from "uuid";
 import moment from "moment";
-import { createGlobalStyle } from 'styled-components'
+import GlobalStyle from './GlobalStyle'
 import 'antd/dist/antd.css';
 import { Layout, Row, Col } from 'antd';
 
 const { Header, Footer, Content } = Layout;
-
-const GlobalStyle = createGlobalStyle`
-    body {
-        margin: 0;
-        font-family: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-        font-size: 1rem;
-        font-weight: 400;
-        line-height: 1.5;
-        color: #212529;
-        text-align: left;
-        background-color: #e6ecf0;
-    }
-`;
 
 class App extends React.Component {
 
     constructor(props) {
         super(props);
 
-        /* posts from DB */
-        this.dateFromDatabase = 1572605803000;
         this.avatar = require("./avatar.png");
         this.state = {
             posts: [
-                { id: "2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d'", avatar: this.avatar, fullname: 'Developer', message: 'First tweet', username: '@developer', time: moment(this.dateFromDatabase).fromNow() },
-                { id: "9125a8dc-52ee-365b-a5aa-81b0b3681cf6", avatar: this.avatar, fullname: 'Developer', message: 'Second tweet', username: '@developer', time: moment(this.dateFromDatabase).fromNow() },
-                { id: "c6235813-3ba4-3801-ae84-e0a6ebb7d138", avatar: this.avatar, fullname: 'Developer', message: 'Third tweet', username: '@developer', time: moment(this.dateFromDatabase).fromNow()}
+                { id: "2c5ea4c0-4067-11e9-8bad-9b1deb4d3b7d", avatar: this.avatar, fullname: 'Developer', message: 'First tweet', username: '@developer', time: moment(1572605803000).fromNow() },
+                { id: "9125a8dc-52ee-365b-a5aa-81b0b3681cf6", avatar: this.avatar, fullname: 'Developer', message: 'Second tweet', username: '@developer', time: moment(1572605803000).fromNow() },
+                { id: "c6235813-3ba4-3801-ae84-e0a6ebb7d138", avatar: this.avatar, fullname: 'Developer', message: 'Third tweet', username: '@developer', time: moment(1572605803000).fromNow()}
             ]
         };
     }
 
     addPost = (message) => {
-        const   date = new Date(),
-                time = moment(date).fromNow(),
-                id = uuid.v1(),
-                avatar = require("./avatar.png"),
-                post = {
-                    id,
-                    avatar,
-                    fullname: 'User',
-                    message,
-                    username: '@user',
-                    time
-                };
+        const post = {
+            id: uuid.v1(),
+            avatar: require("./avatar.png"),
+            fullname: 'User',
+            message,
+            username: '@user',
+            time: moment(new Date()).fromNow()
+        };
 
         const posts = [ ...this.state.posts, post ];
         this.setState( { posts });
