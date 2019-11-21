3f8fd1a88c45c4f609a8dfc4f7423aae6859c0e7
mchdsnv <michail.chudesnow2@gmail.com>
2019-11-21 11:33:03 +0200

    Postman collection was added

--- api/app/Http/Controllers/PostController.php
+++ api/app/Http/Controllers/PostController.php
@@ -46,8 +46,7 @@ class PostController extends Controller
      */
     public function show($id)
     {
-        //return Post::findOrFail($id);
-        $posts = Post::findOrFail($id);
+        return Post::findOrFail($id);;
     }
 
     /**
