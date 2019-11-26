5eb4390047049793850695951fb4b7b532c7b217
mchdsnv <michail.chudesnow2@gmail.com>
2019-11-26 15:29:19 +0200

    React was updated

--- web/src/store/twitter/twitter-actions.js
+++ web/src/store/twitter/twitter-actions.js
@@ -148,3 +148,31 @@ export const updCounter = (counter) => ({
         counter: counter
     }
 });
+
+export const LOGIN_REQUEST = 'LOGIN_REQUEST';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const userLogin = () => {
+    return {
+        //
+    }
+};
+
+export const LOGOUT = 'USERS_LOGOUT';
+
+export const userLogout = () => {
+    return {
+        //
+    }
+};
+
+export const SIGNUP_REQUEST = 'SIGNUP_REQUEST';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_FAILURE = 'SIGNUP_FAILURE';
+
+export const userSignup = () => {
+    return {
+        //
+    }
+};
